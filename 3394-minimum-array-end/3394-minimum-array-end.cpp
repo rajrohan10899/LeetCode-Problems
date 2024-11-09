f2606074f890805c550b1d69c55bf2aab2d2727c
Rohan Raj <76591097+rajrohan10899@users.noreply.github.com>
2024-11-09 12:25:45 +0530

    Time: 1754 ms (5.36%), Space: 8.6 MB (52.8%) - LeetHub

---
+++ 3394-minimum-array-end/3394-minimum-array-end.cpp
@@ -0,0 +1,11 @@
+typedef long long ll;
+class Solution {
+public:
+    long long minEnd(int n, int x) {
+        ll res = x;
+        for(int i = 1; i < n; i++) {
+            res = res+1 | x; 
+        }
+        return res;
+    }
+};
