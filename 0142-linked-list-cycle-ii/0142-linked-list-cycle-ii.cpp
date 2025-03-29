465aaff9166246641fd5c3e0b73ebda8967f1a90
Rohan Raj <76591097+rajrohan10899@users.noreply.github.com>
2025-03-29 12:40:14 +0530

    Time: 5 ms (84.02%), Space: 11.2 MB (98.59%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,14 +9,23 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        ListNode* temp = head;
-        unordered_map<ListNode*, int>mpp;
-        while(temp != NULL) {
-            if(mpp.find(temp) != mpp.end()) {
-                return temp;
+        ListNode* slow_P = head;
+        ListNode* fast_P = head;
+
+        while(fast_P != NULL && fast_P->next != NULL) {
+            slow_P = slow_P->next;
+            fast_P = fast_P->next->next;
+
+            //1. Detect Loop:
+            if(slow_P == fast_P) { // Loop is there
+                //2. Step : Find Starting Point.
+                slow_P = head;
+                while(slow_P != fast_P) {
+                    slow_P = slow_P->next;
+                    fast_P = fast_P->next;
+                }
+                return slow_P;
             }
-            mpp[temp] = 1;
-            temp = temp->next;
         }
         return NULL;
     }
