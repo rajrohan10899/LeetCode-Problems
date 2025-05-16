66433acf2440f597fb92dc2d299a35465f0b29c6
Rohan Raj <76591097+rajrohan10899@users.noreply.github.com>
2025-05-17 00:43:58 +0530

    Time: 0 ms (100%), Space: 13.5 MB (97.82%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        // Brute-Force:
-        for(int i = 0; i < nums.size(); i++) {
-            if(nums[i] == target) {
-                return i; // target mil gaya 
+        // Optimal Approach:
+        int low = 0, high = nums.size()-1, res = nums.size();
+        while(low <= high) {
+            int mid = (low + high)/2;
+
+            if(nums[mid] == target) {
+                return mid;
             }
-            else if(nums[i] > target) {
-                return i; // yahan insert kar sakte ho
+            else if(nums[mid] > target) {
+                res = mid;
+                high = mid-1;
+            }
+            else {
+                low = mid+1;
             }
         }
-        return nums.size(); // target sabse bada hai, end me insert hoga
+        return res;
     }
 };
