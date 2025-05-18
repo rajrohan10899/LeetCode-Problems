8bb07eef5ad3cb7c33a654a46c26d27c3130ad11
Rohan Raj <76591097+rajrohan10899@users.noreply.github.com>
2025-05-18 10:57:04 +0530

    Time: 0 ms (100%), Space: 17.6 MB (17.21%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,34 +1,21 @@
-// class Solution {
-// public:
-//     vector<int> searchRange(vector<int>& nums, int target) {
-//         int firstOcc = -1;
-//         int lastOcc = -1;
-//         for(int i = 0; i < nums.size(); i++) {
-//             if(nums[i] == target) {
-//                 if(firstOcc == -1) {
-//                     firstOcc = i;
-//                     lastOcc = i;   
-//                 }
-//                 else {
-//                     lastOcc = i;
-//                 }
-//             }
-//         }
-//         return {firstOcc, lastOcc};
-//     }
-// };
-
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int>res(2, -1);
-        auto lb = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
-        auto ub = upper_bound(nums.begin(), nums.end(), target) - nums.begin()-1;
-
-        if(lb < nums.size() && nums[lb] == target) {
-            res[0] = lb;
-            res[1] = ub;
+        int firstOcc = -1;
+        int lastOcc = -1;
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] == target) {
+                if(firstOcc == -1) {// checking that is it still -1 : skip ? update firstOcc
+                    firstOcc = i;
+                    lastOcc = i;   
+                }
+                else {
+                    lastOcc = i;
+                }
+            }
         }
-        return res;
+        return {firstOcc, lastOcc};
     }
 };
+
+
