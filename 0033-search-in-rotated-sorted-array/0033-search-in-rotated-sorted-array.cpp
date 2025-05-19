a8718cd0490ea6aafd2445862b54c30e8432be3b
Rohan Raj <76591097+rajrohan10899@users.noreply.github.com>
2025-05-19 12:51:38 +0530

    Time: 0 ms (100%), Space: 15.3 MB (33.24%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int low = 0, high = nums.size()-1; 
-        while(low <= high) {
-            int mid = (low + high)/2;
-            if(nums[mid] == target)
+
+        int low = 0, high = nums.size() - 1;
+        
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            // Is the mid equals to target:
+            if (nums[mid] == target) {
                 return mid;
-            if(nums[low] <= nums[mid]) {
-                if(nums[low] <= target && target <= nums[mid]) {
-                    high = mid - 1;
-                }
-                else {
-                    low = mid + 1;
-                }
             }
-            else {
-                if(nums[mid] <= target && target <= nums[high]) {
+
+            // check is the left portion of the array is sorted:
+            if (nums[low] <= nums[mid]) {
+                // Is the target lies in left portion ?.
+                if (nums[low] <= target && target <= nums[mid]) {
+                    high = mid - 1;
+                } else {
                     low = mid + 1;
                 }
-                else {
+            } else {
+                // check is the right portion of the array is sorted:
+                if (nums[mid] <= target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
                     high = mid - 1;
                 }
             }
