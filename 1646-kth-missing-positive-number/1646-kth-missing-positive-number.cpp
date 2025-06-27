9ebf014eda05e14c081b200563760c5f0ed39826
Rohan Raj <76591097+rajrohan10899@users.noreply.github.com>
2025-06-28 03:43:22 +0530

    Time: 0 ms (100%), Space: 13.2 MB (90.12%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
-/*
-You’re checking for how many numbers from 1 to arr[i] are missing.
-
-If arr[i] <= k, that means the current number in the array is not the kth missing number yet, so you increment k (since one missing number is skipped because the array has the number arr[i]).
-
-When arr[i] > k, it means that the k-th missing number lies before arr[i]. So we break and return k.
-*/
     int findKthPositive(vector<int>& arr, int k) {
-        for(int i = 0; i < arr.size(); i++) {
-            if(arr[i] <= k) {
-                k++;
-            }
-            else {
-                break;
+        int low = 0, high = arr.size() - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] - mid - 1 < k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
-        return k;
+        return low + k;
     }
 };
