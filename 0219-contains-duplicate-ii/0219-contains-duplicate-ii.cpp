c68f237ff9c1514e77453ca25e9bcdb12c59af9e
Rohan Raj <76591097+rajrohan10899@users.noreply.github.com>
2025-08-22 23:01:48 +0530

    Time: 81 ms (30.06%), Space: 93.6 MB (86.36%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        int i = 0, j = 0;
         unordered_set<int>st;
+        int i = 0, j = 0;
         while(j < nums.size()) {
+            //Invalid.
             if(abs(i-j) > k) {
+                //window shrink
                 st.erase(nums[i]);
                 i++;
             }
+            //Duplication.
             if(st.find(nums[j]) != st.end()) {
                 return true;
             }
